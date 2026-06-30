5ad4ca2c7f7de8fdfb65e4d13468f6c3b9da3556
Colton Stone <stonekids010204@gmail.com>
2026-06-30 14:57:24 -0400

    finalised infocard.py and fixed scope error for the variable "n"

---
+++ raspberry-pi-foundations/infocard.py
@@ -0,0 +1,41 @@
+import random
+# import keyboard
+
+name = "Colton Stone"
+age = "15"
+grade = "10th"
+interests = "music, gaming, and coding"
+hereBc = "I joined because I wanted a summer job that I was actually interested in, and I'm into computers and stuff."
+goals = "I want to learn to be able to use a Raspberry Pi"
+personality = ["Error: could not locate brain", "Error: could not interpret behaviors", "Error: could not find personality"]
+n = 0
+
+def proceed():
+  global n
+  match n:
+    case 0:
+      print("\nMy name is " + name)
+    case 1:
+      print("\nI'm " + str(age) + " years old")
+    case 2:
+      print("\nI'm in " + grade)
+    case 3:
+      print("\nMy main interests are " + interests)
+    case 4:
+      print("\n" + hereBc)
+    case 5:
+      print("\n" + goals)
+    case 6:
+      print(personality[random.randint(0, 2)])
+    case 7:
+      # print("Press SPACE to exit...")
+      # keyboard.wait("space")
+      input("Press ENTER to exit...")
+      exit()
+  n += 1
+
+while n >= 0 & n <= 6:
+  # print("Press SPACE to continue...")
+  # keyboard.wait("space")
+  input("Press ENTER to continue...")
+  proceed()
