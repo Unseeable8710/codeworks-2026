e1116939344f110f2467025372d7068697650d4a
Colton Stone <stonekids010204@gmail.com>
2026-07-13 10:28:56 -0400

    fixed trafficlight.py

--- raspberry-pi-foundations/trafficlight.py
+++ raspberry-pi-foundations/trafficlight.py
@@ -5,17 +5,19 @@ red = LED(17)
 yellow = LED(27)
 green = LED(22)
 
-red.off()
-yellow.off()
-green.off()
+# green.off()
 
 while True:
+  red.off()
+  yellow.off()
   green.on()
   sleep(3)
   green.off()
+  
   yellow.on()
   sleep(1)
   yellow.off()
+  
   red.on()
   sleep(3)
   red.off()
