594854e2006e6ea7a1a557097107faaa07162671
Colton Stone <stonekids010204@gmail.com>
2026-07-14 12:00:32 -0400

    created note calculator

--- raspberry-pi-foundations/btnsnd.py
+++ raspberry-pi-foundations/btnsnd.py
@@ -1,6 +1,42 @@
-from gpiozero import Button
+from gpiozero import Button, TonalBuzzer
+from gpiozero.tones import Tone
+from time import sleep
 
 button = Button(2)
+buzzer = TonalBuzzer(17)
+tempo = 180
+whole = (6000 * 4) / tempo
+
+def note(n: str, o: int):
+  global f
+  n = n.lower()
+  if n == "a":
+    f = 440 * 2^(-4)
+  elif n == "bb":
+    f = 440 * 2^(-47/12)
+  elif n == "b":
+    f = 440 * 2^(-23/6)
+  elif n == "c":
+    f = 440 * 2^(-45/12)
+  elif n == "cs":
+    f = 440 * 2^(-11/3)
+  elif n == "d":
+    f = 440 * 2^(-43/12)
+  elif n == "eb":
+    f = 440 * 2^(-9/2)
+  elif n == "e":
+    f = 440 * 2^(-41/12)
+  elif n == "f":
+    f = 440 * 2^(-10/3)
+  elif n == "fs":
+    f = 440 * 2^(-13/6)
+  elif n == "g":
+    f = 440 * 2^(-19/6)
+  elif n == "gs":
+    f = 440 * 2^(-37/12)
+  
+  return 2 * f * (o + 1)
+  
 
 button.wait_for_press()
 print("pressed")
--- raspberry-pi-foundations/flaskproject/flaskapp.py
+++ raspberry-pi-foundations/flaskproject/flaskapp.py
@@ -12,34 +12,34 @@ led6 = LED(17)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 @app.route('/control', methods=['POST'])
 def control():
-    led = request.form.get('led')
-    action = request.form.get('action')
+  led = request.form.get('led')
+  action = request.form.get('action')
 
-    if led == '1':
-        target = led1
-    elif led == '2':
-        target = led2
-    elif led == '3':
-        target = led3
-    elif led == '4':
-        target = led4
-    elif led == '5':
-        target = led5
-    elif led == '6':
-        target = led6
-    else:
-        return "Invalid LED"
+  if led == '1':
+    target = led1
+  elif led == '2':
+    target = led2
+  elif led == '3':
+    target = led3
+  elif led == '4':
+    target = led4
+  elif led == '5':
+    target = led5
+  elif led == '6':
+    target = led6
+  else:
+    return "Invalid LED"
 
-    if action == 'on':
-        target.on()
-    elif action == 'off':
-        target.off()
+  if action == 'on':
+    target.on()
+  elif action == 'off':
+    target.off()
 
-    return render_template('index.html')
+  return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5000)
